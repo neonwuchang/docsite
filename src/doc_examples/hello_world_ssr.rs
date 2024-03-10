fa9b0c944a1768e7c4d57af46311ee01ab217190
neonwuchang <103783716+neonwuchang@users.noreply.github.com>
2024-03-09 18:52:55 -0800

    Update hello_world_ssr.rs

--- src/doc_examples/hello_world_ssr.rs
+++ src/doc_examples/hello_world_ssr.rs
@@ -37,7 +37,7 @@ async fn app_endpoint() -> Html<String> {
 // ANCHOR: second_endpoint
 async fn second_app_endpoint() -> Html<String> {
     // create a component that renders a div with the text "hello world"
-    fn app() -> Element {
+    fn App() -> Element {
         rsx!( div { "hello world" } )
     }
     // create a VirtualDom with the app component
